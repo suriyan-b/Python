a096ac3972d793b89ed072288a3fedd39956cfcd
suriyan-b <131989515+suriyan-b@users.noreply.github.com>
2024-02-29 18:21:44 +0530

    Add files via upload

---
+++ Creating a variable and using operators.py
@@ -0,0 +1,15 @@
+print("Hello World")  #my hython program
+#creating a variable 
+a="vishnu"
+b="suriyan"
+print(b) #suriyan
+
+d=2
+b=27.77
+print(d+b) #29.77
+print(d-b) #-25.77
+print(d*b)#55.54
+print(d/b)#0.07202016564638099
+print(d**b)#228877435.97324204
+print(d%b)#2.0
+print(d//b)#0.0
